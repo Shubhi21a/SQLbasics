2ede7a5360d871cbb5a8d2ed7d6ec5ba96927f1d
Shubhi Ahuja <39938214+Shubhi21a@users.noreply.github.com>
2021-01-04 22:23:09 +0530

    Add files via upload
    Basic SQL practice + dense rank + partition by

---
+++ Dense_Rank Partition_by.sql
@@ -0,0 +1,45 @@
+-- viewing the table
+select top 5 * from [dbo].[Employees]
+
+-- max salary by Gender
+select gender, max(salary) from [dbo].[Employees]
+group by gender
+
+-- dense rank by salary
+select *, DENSE_RANK() over (order by salary desc) as denseRank
+from [dbo].[Employees]
+
+-- Records with 4th highest salary
+select * from
+(select *, dense_rank() over (order by salary desc) as denseRank from dbo.Employees)ss
+where denseRank = 4
+
+-- 4th highest salary value
+select distinct salary from (
+select *, DENSE_RANK() over (order by salary desc) as denseRank from dbo.Employees) ss
+where denseRank = 4
+
+-- Seeing dense rank by Gender field
+select *, DENSE_RANK() over (partition by gender order by salary desc) as denseRank
+from [dbo].[Employees]
+
+-- records with 2nd highest salary for each gender
+select * from (select *, DENSE_RANK() over (partition by gender order by salary desc) as denseRank
+from [dbo].[Employees])ss
+where denseRank = 2
+
+-- 2nd highest salary value by Gender
+select distinct gender,salary  from
+(select *, DENSE_RANK() over (partition by gender order by salary desc) as denseRank
+from [dbo].[Employees])ss
+where denseRank = 2
+
+--- Dense rank for distinct records (after excluding ID column)
+select *, DENSE_RANK() over (partition by gender order by salary desc) as denseRank from
+(select distinct FirstName, LastName, Gender,Salary from dbo.Employees)ss
+
+-- Record detail for 2nd highest salary, post distinct
+select * from 
+(select *, DENSE_RANK() over (partition by gender order by salary desc) as denseRank from
+(select distinct FirstName, LastName, Gender,Salary from dbo.Employees)ss)tt
+where denseRank = 2
